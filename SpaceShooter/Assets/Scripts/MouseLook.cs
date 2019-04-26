d3f65db220e22037b0c3d10ffc684fcc0d3e784d
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-04-26 10:33:49 +0300

    + MouseLook

---
+++ SpaceShooter/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivityHor = 9.0f; // переменная для скорости вращения
+    public float sensitivityVert = 9.0f; // переменная для скорости по вертикали
+    public float minimumVert = -45.0f; //минимальный угол по вертикали
+    public float maximumVert = 45.0f; // максимальный угол по вертикали
+    private float _rotationX = 0; //закрытая переменная для сохраннеия угла поворота вертикали
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float delta = Input.GetAxis("Mouse X") * sensitivityHor; //Новая переменная хранит считываемое изменение мыши по Х
+
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed == 6.0f)
+        {
+            transform.Rotate(0, speed, 0);
+        }
+        else
+        {
+            transform.Rotate(0, -speed, 0);
+        }
+
+    }
+}
